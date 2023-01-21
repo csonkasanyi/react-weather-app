db9c700cc90a15979e0e7fa4d313db7eb7765503
Csonka Sándor <csonkasanyi2@gmail.com>
2023-01-21 18:11:41 +0100

    Xaxis removed from imports.

--- src/Components/Forecast.jsx
+++ src/Components/Forecast.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { LineChart, Line, CartesianGrid, YAxis, Tooltip } from 'recharts';
 import '../styles/Forecast.css'
 
 const Forecast = ({ weatherForecast }) => {
